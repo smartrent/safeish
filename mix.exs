4095ce0ff50e3bb4bc43d1d3833f481de72ede29
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2021-07-07 15:38:19 -0400

    Release v0.5.0-smartrent.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Safeish.MixProject do
   def project do
     [
       app: :safeish,
-      version: "0.5.0",
+      version: "0.5.0-smartrent.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: description(),
@@ -26,11 +26,11 @@ defmodule Safeish.MixProject do
   defp deps do
     [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
   end
-  
+
   defp description() do
     "NOT FOR PRODUCTION USE: Safe-ish is an experimental sandbox for BEAM modules that examines and rejects BEAM bytecode containing instructions that could cause side effects. You can provide an optional whitelist of opcodes and functions the module can use."
   end
-  
+
   defp package() do
     [
       name: "safeish",
@@ -41,24 +41,24 @@ defmodule Safeish.MixProject do
       maintainers: ["Robin Hilliard"]
     ]
   end
-  
+
   defp aliases, do: [fixtures: &make_fixtures/1]
-  
+
   defp make_fixtures(_) do
     source_path =  "#{File.cwd!()}/test/fixtures.ex"
     build_path = "#{File.cwd!()}/test/fixtures_build"
-    
+
     Mix.Shell.IO.info("Compiling modules in #{source_path} and saving to #{build_path}")
-    
+
     File.rm_rf(build_path)
     File.mkdir(build_path)
-    
+
     for {module, bytecode} <- Code.compile_file(source_path) do
       Mix.Shell.IO.info("...#{build_path}/#{Atom.to_string(module)}.beam")
       {:ok, file} = File.open("#{build_path}/#{Atom.to_string(module)}.beam", [:write])
       IO.binwrite(file, bytecode)
       File.close(file)
     end
-    
+
   end
 end
