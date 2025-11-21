2a876667b5e782ac1bc339afb5c7adbcf4295b86
Jean-Francois Cloutier <jean-francois.cloutier@smartrent.com>
2025-11-21 13:36:37 -0500

    ditto

--- lib/safeish.ex
+++ lib/safeish.ex
@@ -705,8 +705,9 @@ defmodule Safeish do
   """
   def check(bytecode, whitelist \\ []) do
     {:ok, module, risks} = module_risks(bytecode)
+    IO.puts("@@@ MODULE RISKS #{inspect module_risks}")
     check_list = risks |> Enum.map(&risk_acceptable?(&1, whitelist))
-
+    IO.puts("@@@ CHECK_LIST #{inspect check_list}")
     if Enum.all?(check_list, &match?(:ok, &1)) do
       {:ok, module}
     else
